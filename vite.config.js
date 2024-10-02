3a849d4ad5057d4bf256df6d4bd4aa8e5eb47abd
manjushaaa20 <manjushaacharya919@gmail.com>
2024-10-02 21:39:58 +0530

    Initial commit - Frontend

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Base URL for deployment
+  base: '/grocery-management/', // Replace with your GitHub repository name if deploying on GitHub Pages
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Output directory for the build files
+    sourcemap: true, // Generate source maps for easier debugging
+  },
+  server: {
+    open: true, // Automatically open the app in the browser when running the dev server
+  },
+});
